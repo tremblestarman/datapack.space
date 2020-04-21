5fe8c55543e259796b27e894cd2e29adc7428db0
tremblestarman <tremblestarman@gmail.com>
2020-04-21 22:08:25 +0800

    2020.4.21

--- bin/js/ender-wanderer.js
+++ bin/js/ender-wanderer.js
@@ -1,31 +1,85 @@
-let elements = document.querySelectorAll('.tags .tag-2, .tags .tag-3')
-let statistic = {}, rank = [], datapacks = [];
-for (let i = 0; i < elements.length; i++) {
-    if (statistic.hasOwnProperty(elements[i].textContent)) {
-        statistic[elements[i].textContent].v++;
-        elements[i].parentNode.removeChild(elements[i]);
-    }
-    else statistic[elements[i].textContent] = { v: 1, i: i};
-}
-Array.prototype.forEach.call(Object.keys(statistic).sort(function(a,b) {
-        return statistic[b].v-statistic[a].v
-    }), function (k) {
-    rank.push({
-        el: elements[statistic[k].i],
-        c: statistic[k].v
-    })
-});
-Array.prototype.forEach.call(document.querySelectorAll('.name'), function (d) {
-    datapacks.push({
-        name: d.textContent,
-        id: d.id
+let statistic = {}, // name -> id
+    tags = [],      // { c: count, r: raw html, s: sub node count } (sort by count)
+    datapacks = [], // { n: name, u: uid }
+    relations = []; // [][] relation table
+let sum = 0, root = null;
+function getTagsInDatapack(d) {
+    let elements = []
+    Array.prototype.forEach.call(d.children[1].children, function (t) {
+        if (t.classList.contains("tag-2") || t.classList.contains("tag-3")) {
+            elements.push(t);
+        }
     });
-})
-let root =  null;
+    return elements;
+}
+Array.prototype.forEach.call(document.querySelectorAll('.datapack-short'), function (d) {
+    elements = getTagsInDatapack(d)
+    for (let i = 0; i < elements.length; i++) {
+        if (statistic.hasOwnProperty(elements[i].textContent)) {
+            tags[statistic[elements[i].textContent]].c++;
+        }
+        else {
+            statistic[elements[i].textContent] = sum;
+            tags.push({
+                c: 1,
+                r: elements[i],
+                s: 0
+            })
+            sum++;
+        }
+    }
+    datapacks.push({
+        n: d.children[0].textContent,
+        u: d.children[0].id,
+        t: d.children[1]
+    });
+}); // read data
+tags.sort(function(a,b) {
+    return b.c-a.c;
+}) // set rank
+for (let i = 0; i < sum; i++) {
+    statistic[tags[i].textContent] = i;
+} // update index of statistics, inverted
+for (let i = 0; i < sum; i++) {
+    relations[i] = [];
+    for (let j = 0; j < sum; j++)
+        relations[i][j] = 0;
+} // init relation table
+Array.prototype.forEach.call(document.querySelectorAll('.datapack-short'), function (d) {
+    elements = getTagsInDatapack(d)
+    for (let i = 0; i < elements.length; i++) {
+        let vi = statistic[elements[i].textContent];
+        for (let j = 0; j < elements.length; j++) {
+            if (elements[j] !== elements[i]) {
+                let vj = statistic[elements[j].textContent];
+                if (vi < vj)
+                    relations[vi][vj]++;
+            }
+        }
+    }
+}); // build relation table in partial order
+for (let i = 0; i < sum; i++) {
+    for (let j = 0; j < sum; j++) {
+        for (let k = 0; k < sum; k++) {
+            if (relations[i][k] > 0 && relations[k][j] > 0) {
+                relations[i][j] = 0;
+            }
+        }
+        if (relations[i][j] > 0) {
+            tags[i].s++;
+        }
+    }
+    console.log(tags[i].r.textContent, tags[i].s, relations[i].join(" "));
+} // hasse it
+
 Array.prototype.forEach.call(document.querySelectorAll('.tag-unique'), function (e) {
     root = e.parentNode;
-    root.removeChild(e); // Clear All
-});
+    root.removeChild(e);
+}); // remove all
+tags = document.createElement("canvas")
+tags.id = "tags";
+root.appendChild(tags); // add tags panel
 datapacks = document.createElement("div")
-datapacks.id = "datapacks";
+datapacks.id = "datapacks"; // add datapacks panel
 root.appendChild(datapacks);
+
--- robot.sh
+++ robot.sh
@@ -6,3 +6,4 @@ xvfb-run /usr/local/bin/python3 -u "$parent_path"/update.py > "$parent_path"/rob
 cd util || exit
 ./indexer > indexer.log 2>&1
 mysqldump -u root -p$MYSQLPASSWORD datapack_collection -P 3306 datapacks tags authors datapack_tags datapacks_related authors_related datapacks_log datapacks_ii_queue > /TheEndOfDatapack/datapack_collection.sql
+ps aux | grep "/update.py" |  awk '{print $2}' | xargs kill -9
